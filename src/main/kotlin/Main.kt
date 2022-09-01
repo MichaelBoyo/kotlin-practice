19b99ca411749bb984c60f3f9cca9a6d9ac240c5
MichaelBoyo <boyomichaelbidemi@gmail.com>
2022-09-01 01:10:07 +0100

    kt

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+import kotlin.reflect.typeOf
+
+fun main() {
+
+//    val userIn = readLine()?: "0"
+//    val userIn2 = readLine()?: "0"
+//    val result = userIn.toInt() + userIn2.toInt()
+
+    val shoppingList = mutableListOf("Eggs", "Milk", "Bread", "Apples")
+    shoppingList.add("Bananas")
+    println(shoppingList[4])
+
+
+
+}
